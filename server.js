b779cbba6c3490a4e5fef765fe59dc9d1007a88b
inigokd <bella.uglava@gmail.com>
2019-03-28 17:55:54 +0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const fs = require("fs");
+const morgan = require("morgan");
+const cors = require("cors");
+const apiRoutes = require("./routes/apiRoutes");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "./view");
+
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+
+
+
+app.all('/', (req, res) => {
+  const params = {
+    title: 'Main Page',
+    page: 'home',
+    error: '',
+    success: '',
+    statusColor: ''
+  }
+
+  if (req.body.username !== undefined) {
+    // check and add user
+    Users.forEach(user => {
+      if (user.username === req.body.username) {
+        params.error = 'Username already exists';
+      }
+    });
+
+    if (req.body.username.length < 3 || req.body.password < 3) {
+      params.error = 'Username or password is too short.';
+    }
+
+    if (params.error === '') {
+      const userInfo = {
+        username: req.body.username,
+        email: req.body.email,
+        phone: req.body.phone,
+        password: req.body.password
+      };
+      const username = req.cookies.Users;
+      Users.push(userInfo);
+
+      params.success = 'User added successfully';
+      params.statusColor = 'green';
+    } else {
+      params.statusColor = 'red';
+    }
+  }
+
+  res.render('main', params);
+});
+
+
+//
+app.all('/users', (req, res) => {
+  const params = {
+    title: 'List of Users',
+    page: 'users',
+    error: 'Wrong Username or password',
+    success: '',
+    statusColor: 'red'
+  }
+
+  Users.forEach(user => {
+    if (user.username === req.body.username) {
+      if (user.password === req.body.password) {
+        params.success = `Welcome ${user.username}!`;
+        params.error = '';
+        params.statusColor = 'green';
+      }
+    }
+  });
+
+  res.render('main', params);
+});
+
+// app.use("/api/v1", apiRoutes);
+// app.use("/books", bookRoutes);
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "view/", "product.ejs"));
+});
+
+app.listen(PORT, () => {
+    console.log('http://localhost:'  +  PORT)
+})
